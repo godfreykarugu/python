1d03877e2cdde232bf6a91660c313fb59426b8b1
godfreykarugu <godfreykarugu77@gmail.com>
2024-04-02 21:36:25 -0700

    add whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,26 @@
+'''i=1
+while i<=10:
+    if i == 3:
+        break
+    print(i)
+    i=i+ 1'''
+
+'''i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)'''
+# ------------------ using step  -----------------------------------------------
+'''count = 1
+while count < 50:
+  print(count ,end=' ')
+  count = count + 2'''
+
+#----------------- using modulus ------------------
+count = 1
+while count < 50:
+  if count % 2 != 0:
+    print(count,end=' ')
+  count = count + 1
+
